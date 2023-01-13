dc8a227540e0e9dbe0abc40bf2ea7a894630daa6
mbh <mbh@electronic-minds.de>
2023-01-13 10:53:47 +0100

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./App.module.css";
+
+function App() {
+    return (
+        <>
+            <h1>Ich bin die App Componente</h1>
+            <p>bla</p>
+        </>
+    );
+}
+
+export default App;
